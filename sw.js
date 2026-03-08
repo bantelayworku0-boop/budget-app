fbff574de7ccf82bde5c71da7d06b4a0274d0094
bantelayworku0-boop <bantelayworku0@gmail.com>
2026-03-08 07:31:25 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,1 @@
+self.addEventListener('install',e=>{e.waitUntil(caches.open('v1').then(c=>c.addAll(['./','/budget-app/','/budget-app/index.html'])))});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
